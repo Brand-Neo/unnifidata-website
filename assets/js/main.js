07305a3ce80a29d2cc143e405a178122ba60599c
Mabweh Kwasen <imabweh@gmail.com>
2022-05-08 17:32:07 +0800

    Add modal and footer image

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,34 @@
+/* ======== pop-up related ======== */
+// check in cookies that user has not closed popup, else open the "Buy Now" popUp, then set cookie accordingly
+function checkCookie() {
+    $('#buyNowPopUp').modal();
+    // let popUpClosed = getCookie("popUpClosed");
+    // if (popUpClosed != "true") {
+    //     $('#buyNowPopUp').modal();
+    //     setCookie("popUpClosed", true, 10);
+    // }
+
+    $('#buyNowPopUpBtn').on('click', (e) => {
+        e.preventDefault();
+        console.log('Buy now clicked');
+        console.log(document.getElementsByClassName("shopify-buy__btn"));
+        document.getElementsByClassName("shopify-buy__btn  ")[0].click();
+    })
+}
+
+
 /* ======== NAVBAR ======== */
 const headerToggle = document.getElementById('header-toggle'),
-      navClose = document.getElementById('nav-close'),
-      navMenu = document.getElementById('nav-menu')
+    navClose = document.getElementById('nav-close'),
+    navMenu = document.getElementById('nav-menu')
 
-if(headerToggle){
+if (headerToggle) {
     headerToggle.addEventListener('click', () => {
         navMenu.classList.toggle('show-nav')
     })
 }
 
-if(navClose){
+if (navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-nav')
     })
@@ -17,17 +36,17 @@ if(navClose){
 
 /* ======== NAVBAR SUBMENU ======== */
 const productList = document.getElementById('product-list'),
-      productSubmenu = document.getElementById('product-submenu'),
-      navChevron = document.getElementById('nav-chevron')
+    productSubmenu = document.getElementById('product-submenu'),
+    navChevron = document.getElementById('nav-chevron')
 
-if(productList){
+if (productList) {
     productList.addEventListener('click', () => {
         productSubmenu.classList.toggle('active')
         navChevron.classList.toggle('fa-chevron-up')
     })
 }
 
-if(productSubmenu){
+if (productSubmenu) {
     productSubmenu.addEventListener('mouseover', () => {
         productSubmenu.classList.add('active')
     })
@@ -35,44 +54,44 @@ if(productSubmenu){
 
 /* ======== COUNTRIES MODAL ======== */
 const countryBtn = document.getElementById('countries-btn'),
-      countryModal = document.getElementById('countries-modal'),
-      countryClose = document.getElementById('countires-close'),
-      openCountry = document.getElementById('open-country')
+    countryModal = document.getElementById('countries-modal'),
+    countryClose = document.getElementById('countires-close'),
+    openCountry = document.getElementById('open-country')
 
 let show = false
-if(countryBtn){
+if (countryBtn) {
     countryBtn.addEventListener('click', () => {
         countryModal.classList.toggle('show-modal')
-        if(show===false){
+        if (show === false) {
             countryBtn.innerText = 'HIDE ALL COUNTRIES'
             show = true
-        } else{
+        } else {
             countryBtn.innerText = 'VIEW ALL COUNTRIES'
             show = false
         }
     })
 }
 
-if(countryClose){
+if (countryClose) {
     countryClose.addEventListener('click', () => {
         countryModal.classList.remove('show-modal')
-        if(show===false){
+        if (show === false) {
             countryBtn.innerText = 'HIDE ALL COUNTRIES'
             show = true
-        } else{
+        } else {
             countryBtn.innerText = 'VIEW ALL COUNTRIES'
             show = false
         }
     })
 }
 
-if(openCountry){
+if (openCountry) {
     openCountry.addEventListener('click', () => {
         countryModal.classList.toggle('show-modal')
-        if(show===false){
+        if (show === false) {
             countryBtn.innerText = 'HIDE ALL COUNTRIES'
             show = true
-        } else{
+        } else {
             countryBtn.innerText = 'VIEW ALL COUNTRIES'
             show = false
         }
@@ -81,18 +100,18 @@ if(openCountry){
 
 /* ======== FAQ ACCORDIAN ======== */
 const faqContent = document.getElementsByClassName('faq-content'),
-      faqHeader = document.querySelectorAll('.faq-header')
+    faqHeader = document.querySelectorAll('.faq-header')
 
-function toggleSkills(){
+function toggleSkills() {
     // Get the clicked faqHeader's parentnode's class
     let itemClass = this.parentNode.className
 
-    for(i=0; i<faqContent.length; i++){
+    for (i = 0; i < faqContent.length; i++) {
         faqContent[i].className = 'faq-content faq-close'
     }
 
     // If currently closed and is clicked, then open it by toggling the class 
-    if(itemClass === 'faq-content faq-close'){
+    if (itemClass === 'faq-content faq-close') {
         this.parentNode.className = 'faq-content faq-open'
     }
 }
@@ -103,9 +122,9 @@ faqHeader.forEach((el) => {
 
 // Whatsapp link buttons
 const linkSpan = document.querySelectorAll('.wa-link-btn'),
-      whatsappBtn = document.getElementById('gcaMainButton'),
-      whatsappChat = document.getElementById('gcaMainCard'),
-      contactBtn = document.getElementById('contact-us-btn')
+    whatsappBtn = document.getElementById('gcaMainButton'),
+    whatsappChat = document.getElementById('gcaMainCard'),
+    contactBtn = document.getElementById('contact-us-btn')
 
 linkSpan.forEach((link) => {
     link.addEventListener('click', () => {
@@ -116,3 +135,28 @@ linkSpan.forEach((link) => {
 contactBtn.addEventListener('click', () => {
     whatsappChat.classList.toggle('gcaDisplay')
 })
+
+// Helper functions
+
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+    let name = cname + "=";
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let ca = decodedCookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
