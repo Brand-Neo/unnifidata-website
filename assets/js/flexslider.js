1c01d6cd0560e5babfd4aee222e4529cdfb44a5e
Mabweh Kwasen <imabweh@gmail.com>
2022-06-14 17:05:25 +0800

    Update slider image for mobile devices

--- assets/js/flexslider.js
+++ assets/js/flexslider.js
@@ -12,8 +12,8 @@ $(window).load(function () {
         startAt: 0,                     //Integer: The slide that the slider should start on. Array notation (0 = first slide)
         slideshow: true,                //Boolean: Animate slider automatically
         slideshowSpeed: 5000,           //Integer: Set the speed of the slideshow cycling, in milliseconds
-        animationSpeed: 600,            //Integer: Set the speed of animations, in milliseconds
-        initDelay: 0,                   //{NEW} Integer: Set an initialization delay, in milliseconds
+        animationSpeed: 300,            //Integer: Set the speed of animations, in milliseconds
+        initDelay: 100,                   //{NEW} Integer: Set an initialization delay, in milliseconds
         randomize: false,               //Boolean: Randomize slide order
 
         // Usability features
